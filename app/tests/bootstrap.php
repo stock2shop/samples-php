15bde62fd7c989e9d469643a79ea0c10d50ff42a
flumonion <chris@gomedia.co.za>
2023-01-19 10:03:12 +0200

    example hanging test

---
+++ app/tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Temporal\Testing\Environment;
+
+$environment = Environment::create();
+$environment->start('./rr serve -c .rr.test.yaml -w tests');
+register_shutdown_function(fn () => $environment->stop());
